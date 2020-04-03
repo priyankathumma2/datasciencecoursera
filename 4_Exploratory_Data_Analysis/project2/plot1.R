330c8313e1f3e88ea3a3fe020b2c06b785ea674e
priyankathumma2 <59976726+priyankathumma2@users.noreply.github.com>
2020-04-03 14:06:00 -0400

    Update plot1.R

--- 4_Exploratory_Data_Analysis/project2/plot1.R
+++ 4_Exploratory_Data_Analysis/project2/plot1.R
@@ -15,4 +15,5 @@ totalNEI <- NEI[, lapply(.SD, sum, na.rm = TRUE), .SDcols = c("Emissions"), by =
 barplot(totalNEI[, Emissions]
         , names = totalNEI[, year]
         , xlab = "Years", ylab = "Emissions"
+        
         , main = "Emissions over the Years")
